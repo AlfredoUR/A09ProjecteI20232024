6da2bffcfcdcd920965c1cd809c11752828bb864
JBatekelaEnti <joel.batekela@enti.cat>
2024-06-28 19:56:41 +0200

    Enemies & Prefabs
    Editar comportament d'enemics i prefabs

---
+++ Assets/Scripts/FlyingEnemyScript.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemy : MonoBehaviour
+{
+    public float moveDistance = 5.0f;
+    public float moveSpeed = 2.0f;    
+    public bool moveHorizontally = true; 
+
+    private Vector2 initialPosition;
+    private Vector2 targetPosition;
+    private bool movingToTarget = true;
+
+    void Start()
+    {
+        initialPosition = transform.position;
+        if (moveHorizontally)
+        {
+            targetPosition = new Vector2(initialPosition.x + moveDistance, initialPosition.y);
+        }
+        else
+        {
+            targetPosition = new Vector2(initialPosition.x, initialPosition.y + moveDistance);
+        }
+    }
+
+    void Update()
+    {
+        MoveEnemy();
+    }
+
+    void MoveEnemy()
+    {
+        if (movingToTarget)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+            if (Vector2.Distance(transform.position, targetPosition) < 0.1f)
+            {
+                movingToTarget = false;
+            }
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, initialPosition, moveSpeed * Time.deltaTime);
+
+            if (Vector2.Distance(transform.position, initialPosition) < 0.1f)
+            {
+                movingToTarget = true;
+            }
+        }
+    }
+}
+
